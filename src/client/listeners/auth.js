c2ccc8f6b385670c8df1011576b6690e1bb8872e
bt <>
2020-11-13 09:58:16 +0200

    wip

--- src/client/listeners/auth.js
+++ src/client/listeners/auth.js
@@ -1,39 +1,40 @@
 import { ERR, loadScript, LOG } from '../modules'
-import * as config from '../../../etc/keys/auth.json'
 
 const src = '//www.gstatic.com/firebasejs/8.0.1/firebase.js'
-
-
+const config = {
+  apiKey: 'AIzaSyAZObOxzbgGsH_cTS-NXYHJtdy8asX9rxQ',
+  authDomain: 'maap-test-292714.firebaseapp.com'
+}
 
 const firebase = {}
 
 const authPopUp = () => {
-
+  
   const provider = new firebase.auth.GoogleAuthProvider()
   provider.addScope('https://www.googleapis.com/auth/contacts.readonly')
   firebase.auth().languageCode = 'pt'
-
+  
   firebase
     .auth()
     .signInWithPopup(provider)
     .then(function (result) {
       const token = result.credential.accessToken
       const user = result.user
-
+      
       LOG('authPopUp', { result })
-
+      
     }).catch(function (error) {
-
+    
     const errorCode = error.code
     const errorMessage = error.message
-
+    
     const email = error.email
     const credential = error.credential
-
+    
     LOG('authPopUp', { error })
-
+    
   })
-
+  
 }
 
 const authEmailPassword = ({ email, password }) =>
@@ -42,10 +43,10 @@ const authEmailPassword = ({ email, password }) =>
     .signInWithEmailAndPassword(email, password)
     // .createUserWithEmailAndPassword(email, password)
     .then(() => {
-     // const user = firebase.auth().currentUser
+      // const user = firebase.auth().currentUser
       
-      const user =  firebase.auth().currentUser
-      LOG('signInWithEmailAndPassword',user)
+      const user = firebase.auth().currentUser
+      LOG('signInWithEmailAndPassword', user)
     })
     .catch(function (error) {
       
